9085e08d784eee68286f2a36ea4ccd7fcc5d8763
andersonsbenicio <andersonsbenicio@gmail.com>
2024-09-05 16:04:13 -0300

    adding steps in ci

--- cypress/e2e/api/crud_user.cy.js
+++ cypress/e2e/api/crud_user.cy.js
@@ -1,29 +1,24 @@
 describe("API CRUD Operations Test", () => {
-  let userId; // Variável para armazenar o ID do usuário criado
-
+  let userId;
   beforeEach(() => {
-    // Carrega os dados do fixture antes de cada teste
     cy.fixture("user_data").as("user_data");
   });
 
   it("should create a new user successfully (POST)", function () {
-    // Gera um e-mail único para cada execução do teste
     const uniqueEmail = `john.doe.${Date.now()}@example.com`;
     const newUser = { ...this.user_data, email: uniqueEmail };
 
-    // Usa o comando customizado para criar o usuário
     cy.createUser(newUser).then((response) => {
       expect(response.status).to.eq(201);
       expect(response.body).to.have.property(
         "message",
         "Cadastro realizado com sucesso"
       );
-      userId = response.body._id; // Armazena o ID do usuário criado
+      userId = response.body._id;
     });
   });
 
   it("should retrieve the created user successfully (GET)", () => {
-    // Usa o comando customizado para obter o usuário
     cy.getUser(userId).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property("_id", userId);
@@ -32,18 +27,16 @@ describe("API CRUD Operations Test", () => {
   });
 
   it("should update the user successfully (PUT)", function () {
-    const updatedEmail = `john.doe.updated.${Date.now()}@example.com`; // Gera um e-mail único para atualização
+    const updatedEmail = `john.doe.updated.${Date.now()}@example.com`;
 
-    // Atualiza todos os campos necessários
     const updatedUserData = {
       ...this.user_data,
       nome: "John Doe Updated",
       email: updatedEmail,
-      password: "updatedpassword123", // Atualiza o campo de senha
-      administrador: "false", // Atualiza o campo de administrador
+      password: "updatedpassword123",
+      administrador: "false",
     };
 
-    // Usa o comando customizado para atualizar o usuário
     cy.updateUser(userId, updatedUserData).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property(
@@ -54,7 +47,6 @@ describe("API CRUD Operations Test", () => {
   });
 
   it("should delete the user successfully (DELETE)", () => {
-    // Usa o comando customizado para deletar o usuário
     cy.deleteUser(userId).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property(
--- cypress/e2e/web/register_validation.cy.js
+++ cypress/e2e/web/register_validation.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe("User Registration Error Validation", () => {
   beforeEach(() => {
     cy.visit("/register");
